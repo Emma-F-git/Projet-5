7327c110cb89a77569ea4f408f20215675caaed9
Emma-F-git <emma.fortat@yahoo.fr>
2025-05-06 10:28:22 +0200

    Modification collapse affichage

--- src/components/ui/Collapse.jsx
+++ src/components/ui/Collapse.jsx
@@ -1,16 +1,29 @@
 import React, { useState } from "react";
 import "../../styles/Collapse.scss";
+import "../../index.css";
 
 function Collapse({ title, children }) {
   const [isOpen, setIsOpen] = useState(false);
 
+  function handleToggle() {
+    setIsOpen((prevIsOpen) => !prevIsOpen);
+  }
+
   return (
     <div className="collapse">
-      <div className="collapse-header" onClick={() => setIsOpen(!isOpen)}>
+      <button
+        className="collapse-header"
+        onClick={handleToggle}
+        aria-expanded={isOpen}
+        aria-controls="collapse-content"
+      >
         <h3>{title}</h3>
         <span className={`arrow ${isOpen ? "open" : ""}`}>▼</span>
-      </div>
-      <div className={`collapse-content ${isOpen ? "open" : ""}`}>
+      </button>
+      <div
+        id="collapse-content"
+        className={`collapse-content ${isOpen ? "open" : ""}`}
+      >
         <p>{children}</p>
       </div>
     </div>
