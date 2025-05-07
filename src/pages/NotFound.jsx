7f4bca43044b3ac0d284b053dd1eeda20d506792
Emma-F-git <emma.fortat@yahoo.fr>
2025-05-07 15:03:52 +0200

    Modification affichage page Not Found

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,12 +1,17 @@
 import "../styles/Header.scss";
+import "../styles/NotFound.scss";
 import { Link } from "react-router-dom";
 
 function NotFound() {
   return (
     <div>
-      <h1 id="error">404</h1>
-      <h2 id="errorMessage">Oups! La page que vous demandez n'existe pas.</h2>
-      <Link to="/">Retourner sur la page d'accueil</Link>
+      <h1 id="notFound">404</h1>
+      <h2 id="notFoundMessage">
+        Oups! La page que vous demandez n'existe pas.
+      </h2>
+      <Link id="linkNotFound" to="/">
+        Retourner sur la page d'accueil
+      </Link>
     </div>
   );
 }
