0b287946c50d706ee0e6b0732e48ed08f118e53c
Emma-F-git <emma.fortat@yahoo.fr>
2025-05-07 11:22:25 +0200

    Modification de l'affichage page 404

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,12 @@
+import "../styles/Header.scss";
+import { Link } from "react-router-dom";
+
 function NotFound() {
   return (
     <div>
-      <h1>404 - Page non trouvée</h1>
-      <p>Oups! La page que vous demandez n'existe pas.</p>
+      <h1 id="error">404</h1>
+      <h2 id="errorMessage">Oups! La page que vous demandez n'existe pas.</h2>
+      <Link to="/">Retourner sur la page d'accueil</Link>
     </div>
   );
 }
