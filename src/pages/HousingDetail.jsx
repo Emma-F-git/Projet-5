8b5a3e29071092129bf0b9c53fc39888c541e0dc
Emma-F-git <emma.fortat@yahoo.fr>
2025-05-08 09:50:05 +0200

    Ajout de la page Housing Detail

--- src/pages/HousingDetail.jsx
+++ src/pages/HousingDetail.jsx
@@ -1,12 +1,28 @@
 import { useParams } from "react-router-dom";
+import SlideShow from "../components/layout/SlideShow";
+import data from "../data/HousingData.json";
+import Collapse from "../components/ui/Collapse";
 
 function HousingDetail() {
   const { id } = useParams();
+  const logement = data.find((item) => item.id === id);
+
+  if (!logement) return <p>Logement introuvable.</p>;
 
   return (
     <div>
-      <h1>Détail du logement</h1>
-      <p>ID du logement : {id}</p>
+      <SlideShow pictures={logement.pictures} />
+      <h1>{logement.title}</h1>
+      <Collapse title="Description">{logement.description}</Collapse>
+
+      <Collapse title="Équipements">
+        <ul>
+          {logement.equipments.map((equipement, index) => (
+            <li key={index}>{equipement}</li>
+          ))}
+        </ul>
+      </Collapse>
+      {/*A ajouter ville, host, rating... */}
     </div>
   );
 }
