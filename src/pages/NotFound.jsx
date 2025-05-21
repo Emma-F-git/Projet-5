0f1b9a7115752c9ce9bd2a8b2a981a57a4a996c6
Emma-F-git <emma.fortat@yahoo.fr>
2025-05-21 16:46:54 +0200

    Modification de l'affichage du footer en mobile

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,10 +1,9 @@
-import "../styles/Header.scss";
 import "../styles/NotFound.scss";
 import { Link } from "react-router-dom";
 
 function NotFound() {
   return (
-    <div>
+    <div className="not-found container">
       <h1 id="notFound">404</h1>
       <h2 id="notFoundMessage">
         Oups! La page que vous demandez n'existe pas.
